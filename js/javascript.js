e17b3a129855285edad0031f5023cc5dd0814c7a
cintia84 <114425106+cintia84@users.noreply.github.com>
2022-12-01 17:41:17 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+function cartel() {alert("Bienvenidos a mi pagina...!!!")
+}
+function inclusive()  {
+        document.body.style.h1.color="white;"
+    }
+function copy() {
+  document.getElementById("Cintia").innerHTML = "Cintia Alvarez; Cinal84@gmail.com; ©Copyright RosAlva"
+}
+function AmpliarImg(x) {
+  x.style.height = "rem";
+  x.style.width = "18rem";
+}
+function NormalImg(x) {
+  x.style.height = "";
+  x.style.width = "14rem";
+}
